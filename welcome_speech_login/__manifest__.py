1bfc7b78d735acf4c0ecfc19d50803ce99db5857
gautam.bothra <gautam.bothra@sunarctechnologies.com>
2021-05-22 03:07:37 +0530

    initial commit

---
+++ welcome_speech_login/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "welcome_speech_login",
+
+    'summary': """
+        This module is for odoo speech on login of users""",
+
+    'description': """
+        This module is for odoo speech on login of users
+    """,
+
+    'author': "Tech GMB",
+    
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    
+    
+}
