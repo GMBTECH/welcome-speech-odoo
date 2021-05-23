1b7bb1e8274115a7eab24697e209f36228c58687
gautam.bothra <gautam.bothra@sunarctechnologies.com>
2021-05-23 17:57:33 +0530

    changed description and added icon

--- welcome_speech_login/__manifest__.py
+++ welcome_speech_login/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "welcome_speech_login",
+    'name': "welcome Login Speech",
 
     'summary': """
         This module is for odoo speech on login of users""",
 
     'description': """
-        This module is for odoo speech on login of users
+        This module is for text to speech which is used on odoo login page. When a user login to odoo, you will hear a login welcome message.
     """,
 
     'author': "Tech GMB",
